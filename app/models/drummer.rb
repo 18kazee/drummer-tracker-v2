6f91305e1af87a4b6cc2218f37e9f6d116bcc412
18kazee <18kazee2415@gmail.com>
2023-09-28 14:03:03 +0900

    Add: Artistモデルの追加

--- app/models/drummer.rb
+++ app/models/drummer.rb
@@ -3,4 +3,7 @@ class Drummer < ApplicationRecord
   validates :country, presence: true
 
   enum country: {japan: 0, abroad: 1}
+
+  has_many :drummer_artists, dependent: :destroy
+  has_many :artists, through: :drummer_artists
 end
---
+++ db/migrate/20230928044524_create_drummer_artists.rb
@@ -0,0 +1,10 @@
+class CreateDrummerArtists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :drummer_artists do |t|
+      t.references :drummer, null: false, foreign_key: true
+      t.references :artist, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
