db7386a962084d2603379d3f55c33611e77547cb
18kazee <18kazee2415@gmail.com>
2023-09-28 18:39:57 +0900

    Add: drummerにincludesメソッド追加

--- app/controllers/drummers_controller.rb
+++ app/controllers/drummers_controller.rb
@@ -1,6 +1,6 @@
 class DrummersController < ApplicationController
   def index
-    @drummers = Drummer.all.page(params[:page])
+    @drummers = Drummer.includes(:artists, :genres).all.page(params[:page]).per(10).order('created_at asc')
   end
 
   def show
