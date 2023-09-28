3f2b287669ac8f5585ed1630cf826b14353e1b1e
18kazee <18kazee2415@gmail.com>
2023-09-29 00:23:58 +0900

    Add: searchアクションとstimulusインスタントサーチ機能とモデルにransac追加

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,8 @@
 class Artist < ApplicationRecord
   has_many :drummer_artists, dependent: :destroy
   has_many :drummers, through: :drummer_artists
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w(album_image album_url created_at id name spotify_id spotify_name updated_at)
+  end
 end
