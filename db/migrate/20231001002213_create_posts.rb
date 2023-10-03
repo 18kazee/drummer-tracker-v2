ed6e15400ccc5081f1744929a26c512853790094
18kazee <18kazee2415@gmail.com>
2023-10-03 09:41:26 +0900

    Add: おすすめ投稿モデルの追加

---
+++ db/migrate/20231001002213_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :tweet
+      t.references :user, null: false, foreign_key: true
+      t.references :drummer, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
