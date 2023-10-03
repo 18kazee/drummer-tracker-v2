24f22c03dfa4d545d0d68166ec74e08cdd6d8d1e
18kazee <18kazee2415@gmail.com>
2023-10-03 09:43:38 +0900

    Add: 投稿カードにマウスオーバー時エフェクト追加

--- app/javascript/scripts.js
+++ app/javascript/scripts.js
@@ -51,4 +51,65 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+    
+    document.addEventListener('turbo:load', () => {
+      const applyPostEffects = (post) => {
+        if (window.location.pathname.includes('/posts/')) {
+          return;
+        }
+
+        post.addEventListener('mouseover', () => {
+          post.style.cursor = 'pointer';
+          post.classList.add('bg-light');
+        });
+
+        post.addEventListener('mouseout', () => {
+          post.style.cursor = 'default';
+          post.classList.remove('bg-light');
+        });
+      };
+
+      const applyUserEffects = (user) => {
+        user.addEventListener('mouseover', () => {
+          user.style.cursor = 'pointer';
+          user.style.opacity = '0.5';  // Add this line to change the opacity
+        });
+
+        user.addEventListener('mouseout', () => {
+          user.style.cursor = 'default';
+          user.style.opacity = '1';  // Add this line to reset the opacity
+        });
+      };
+
+      const applyDrumerEffects = (drummer) => {
+        drummer.addEventListener('mouseover', () => {
+          drummer.style.cursor = 'pointer';
+          drummer.style.opacity = '0.5';  // Add this line to change the opacity
+        });
+        drummer.addEventListener('mouseout', () => {
+          drummer.style.cursor = 'default';
+          drummer.style.opacity = '1';  // Add this line to reset the opacity
+        });
+      };
+
+      document.querySelectorAll('[id^="post-"]').forEach(applyPostEffects);
+      document.querySelectorAll('[id^="post-user-"]').forEach(applyUserEffects);
+      document.querySelectorAll('[id^="post-drummer-"]').forEach(applyDrumerEffects);
+    });
+
+
+    document.addEventListener('turbo:load', function () {
+      if (window.location.pathname.includes('/posts/')) {
+        return;
+      }
+
+        document.querySelectorAll('.card').forEach(function(card) {
+            card.addEventListener('click', function(e) {
+                if (!e.target.closest('.link')) {
+                    window.location.href = card.dataset.href;
+                }
+            });
+        });
+    });
 });
