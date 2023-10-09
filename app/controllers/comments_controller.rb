69c1e8bf1657a0f6acf14d3ba337fab5ef0cc66a
18kazee <18kazee2415@gmail.com>
2023-10-09 23:35:08 +0900

    Add: コメントをbroadcastでactioncable化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,31 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create]
+  before_action :set_comment, only: [:destroy]
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
-    if @comment.save
-      redirect_to post_path(@post), notice: t('defaults.message.created', item: Comment.model_name.human)
-    else
-      flash.now[:alert] = t('defaults.message.not_created', item: Comment.model_name.human)
-      render 'posts/show',status: :unprocessable_entity
-    end
+    return unless @comment.save
+
+    flash.now.notice = t('defaults.message.created', item: Comment.model_name.human)
   end
 
   def destroy
-    @comment = current_user.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@comment.post), notice: t('defaults.message.deleted', item: Comment.model_name.human)
+    flash.now.notice = t('defaults.message.delete', item: Comment.model_name.human)
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = current_user.comments.find(params[:id])
+  end
 
   def comment_params
     params.require(:comment).permit(:body, :post_id, :user_id)
